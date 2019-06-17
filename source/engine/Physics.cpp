1b84f648d9831c7430aa9418624be983cca0464b
Uniday Studio <guilhermegt13@gmail.com>
2019-06-17 10:05:37 -0300

    AABB Collision Detection Refactor
    AABB (Axis-Aligned Bounding Box) collision detection simplified.

--- source/engine/Physics.cpp
+++ source/engine/Physics.cpp
@@ -1,25 +1,16 @@
 #include "Physics.h"
 #include "Entity.h"
 
-static bool Intersect(Vector& a, Vector& endA, Vector& b){
-    if (a.x < b.x && a.y < b.y){
-        if (b.x < endA.x && b.y < endA.y){
-            return true;
-        }
-    }
-    return false;
-}
-
 namespace physics{
     bool AABB(Entity& a, Entity& b){
+        /* Axis-Aligned Bounding Box Algorithm */
+
         Vector endA = a.GetPosition() + a.GetScale();
         Vector endB = b.GetPosition() + b.GetScale();
 
-        if (Intersect(a.GetPosition(), endA, b.GetPosition())){
-            return true;
-        }
-        if (Intersect(b.GetPosition(), endB, a.GetPosition())){
-            return true;
+        if (a.GetPosition().x < endB.x && endA.x > b.GetPosition().x){
+            if (a.GetPosition().y < endB.y && endA.y > b.GetPosition().y)
+                return true;
         }
         return false;
     }
